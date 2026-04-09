290a8ede1df62da1843b5bea8f99c43d3b6f34b3
Viktor <victor.eliseev2@gmail.com>
2026-04-09 18:17:52 +0300

    Исправил и убрал якоря для sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -6,10 +6,6 @@ const sitemap = new SitemapStream({ hostname: 'https://my-english.online' })
 // Перечислите все страницы вашего сайта
 sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 })
 // Дополнительные якоря на существующие секции
-sitemap.write({ url: '/#about', changefreq: 'monthly', priority: 0.8 })
-sitemap.write({ url: '/#process', changefreq: 'monthly', priority: 0.8 })
-sitemap.write({ url: '/#pricing', changefreq: 'monthly', priority: 0.8 })
-sitemap.write({ url: '/#contact', changefreq: 'monthly', priority: 0.8 })
 
 sitemap.end()
 
